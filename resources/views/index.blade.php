ec8221ca64e2d809c442ad082e1ed1402fd1e2b5
KomangArmawan27 <57279299+KomangArmawan27@users.noreply.github.com>
2022-08-22 12:45:07 +0700

    fix icon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,7 +21,7 @@
 
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
-      <img class="animation__wobble" src="/assets/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+      <img class="animation__wobble" src="/assets/img/logo.png" alt="SMKWigaLogo" height="60" width="60">
     </div>
 
     <!-- Navbar -->
