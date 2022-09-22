973f39c4c2f62bfbea109c0b7e90a5b645979aa5
KomangArmawan27 <komang.armawan.27@gmail.com>
2022-09-22 22:01:10 +0700

    update view

--- resources/views/addData.blade.php
+++ resources/views/addData.blade.php
@@ -204,11 +204,8 @@
 
         <!-- Main Footer -->
         <footer class="main-footer">
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+            <strong>Copyright &copy;  <a href="https://smkwidyagama.sch.id/" target="blank">SMK Widyagama</a>.</strong>
             All rights reserved.
-            <div class="float-right d-none d-sm-inline-block">
-                <b>Version</b> 3.2.0
-            </div>
         </footer>
     </div>
     <!-- ./wrapper -->
