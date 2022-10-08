61bbb5606f0d04bdf71ebe3a48c46807a65b5ad1
okymikhael <okymikhael@gmail.com>
2022-10-08 13:29:00 +0700

    update page search

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/admin', function () {
     return view('updateData');
+});
+
+Route::get('/', function () {
+    return view('index');
 });
